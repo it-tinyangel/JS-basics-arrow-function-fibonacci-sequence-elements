2dcbe7e7c8be871d39d643c3ec0966b2d20a4370
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-09-18 00:09:15 +0300

    first n Fibonacci numbers to the console

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const fibonacciSequence = (n) => { // n is the number of Fibonacci numbers that 
 	let a = 1; // the first member of the Fibonacci sequence
 	let b = 1; // the second member of the Fibonacci sequence
 
-	for (let i = 1; i <= n; i++) { // tgeneration of the first n Fibonacci numbers
+	for (let i = 1; i <= n; i++) { // generation of the first n Fibonacci numbers
 		console.log(a); // will output the current value of a to the console
 		const fib = a + b; // the next member of the Fibonacci sequence
 		a = b;
