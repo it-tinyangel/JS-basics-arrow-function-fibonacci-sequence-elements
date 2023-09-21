7f84fb1f542d27ca96fe44325628b4d4c6e13aee
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-09-21 09:51:00 +0300

    first Fibonacci Numbers

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const fibonacciSequence = (n) => { // n is the number of Fibonacci numbers that will be generated
+	let a = 1; // first term of the Fibonacci sequence
+	let b = 1; // second term of the Fibonacci sequence
 
-	let a = 1; // the first member of the Fibonacci sequence
-	let b = 1; // the second member of the Fibonacci sequence
+	console.log(a);
+	console.log(b);
 
-	for (let i = 1; i <= n; i++) { // generation of the first n Fibonacci numbers
-		console.log(a); // will output the current value of a to the console
-		const fib = a + b; // the next member of the Fibonacci sequence
+	for (let i = 3; i <= n; i++) { // generate other Fibonacci numbers
+		const fib = a + b; // next term of the Fibonacci sequence
+		console.log(fib);
 		a = b;
 		b = fib;
 	}
